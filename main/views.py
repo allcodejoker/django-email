ea83aea2924c081c21740b0b6380df9f2930b1b6
WebWrap <149431380+Djole33@users.noreply.github.com>
2025-03-20 19:30:12 +0100

    first commit

---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+
+# Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        your_name = request.POST.get("your-name")
+        your_email = request.POST.get("your-email")
+
+        send_mail(
+            f"Hello, {your_name}",
+            "Hello Inside a message",
+            "allcodejokernewsletter@gmail.com",
+            [your_email],
+            fail_silently=False
+        )
+
+    return render(request, 'main/index.html', {})
